734866459f14250b8696868f0b48c83af14a5d96
Brzydal <damianfilipkowski@gmail.com>
2017-04-03 11:16:19 +0200

    Models for Person and Movie Created

--- REST_Server/movies/models.py
+++ REST_Server/movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    
+    def __str__(self):
+        return '{} {}'.format(self.first_name,self.last_name) 
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    director = models.ForeignKey(Person)
+    actors = models.ManyToManyField(Person,through='Role')
+    year = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.title)
